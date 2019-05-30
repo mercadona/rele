4e9b10c2678a4af1897acaf2159e229ce98d9170
LluisGuiVi <llguirad@mercadona.es>
2019-05-30 11:35:06 +0200

    Add metrics in subscription log (#16)
    * Add metrics to extra key in log
    
    * Take off data from log client publisher

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,7 +8,7 @@ from rele import Publisher
 
 class TestPublisher:
 
-    def test_returns_future_when_published_called(self):
+    def test_returns_future_when_published_called(self, caplog):
         message = {'foo': 'bar'}
         publisher = Publisher()
         publisher._client = MagicMock(spec=PublisherClient)
@@ -21,3 +21,7 @@ class TestPublisher:
         assert isinstance(result, concurrent.futures.Future)
         publisher._client.publish.assert_called_with(
             ANY, b'{"foo":"bar"}', myattr='hello')
+
+        log = caplog.records[0]
+        assert log.message == 'Publishing to order-cancelled'
+        assert log.pubsub_publisher_attrs == {'myattr': 'hello'}
