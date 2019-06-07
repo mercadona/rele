32f95a4d8cc817b142b1c9e259b79cf6ac95783d
Edgar Latorre <egomes@mercadona.es>
2019-06-07 16:42:37 +0200

    Add readthedocs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 html_theme_options = {'font_family':'Heebo'}
+master_doc = 'index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
