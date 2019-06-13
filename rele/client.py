e81ede0907bf81a1042847015ae596a6a6affcba
andrewgy8 <andrewgy8@gmail.com>
2019-06-13 13:52:21 +0200

    More examples in publish function

--- rele/client.py
+++ rele/client.py
@@ -49,18 +49,35 @@ class Publisher:
             self._client = pubsub_v1.PublisherClient(credentials=credentials)
 
     def publish(self, topic, data, blocking=False, **attrs):
-        """Publishes data to Pub/Sub adding a timestamp `published_at` to the attrs.
+        """Publishes message to Google PubSub topic.
 
         Usage::
 
             publisher = Publisher()
             publisher.publish('topic_name', {'foo': 'bar'})
 
+        By default, this method is non-blocking, meaning that the method does not
+        wait for the future to be returned.
+
+        If you would like to wait for the future so you can track the message later,
+        you can:
+
+        Usage::
+
+            publisher = Publisher()
+            future = publisher.publish('topic_name', {'foo': 'bar'}, blocking=True)
+
+        However, it should be noted that using `blocking=True` may incur a significant
+        performance hit.
+
+        In addition, the method adds a timestamp `published_at` to the message attrs
+        using `epoch floating point number <https://docs.python.org/3/library/time.html#time.time>`_.
+
         :param topic: string topic to publish the data.
         :param data: dict with the content of the message.
         :param blocking: boolean, default False.
         :param attrs: Extra parameters to be published.
-        :return: future
+        :return: `Future <https://googleapis.github.io/google-cloud-python/latest/pubsub/subscriber/api/futures.html>`_
         """
 
         attrs['published_at'] = str(time.time())
