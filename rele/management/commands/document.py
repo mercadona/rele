fa789c0af1c7888f1ffcf23f40e5f526773a072d
Daniel Ruiz <druizca@mercadona.es>
2019-06-17 18:51:42 +0200

    Print information about loaded subscriptions in a project

--- rele/management/commands/document.py
+++ rele/management/commands/document.py
@@ -1,9 +1,29 @@
+from django.apps import apps
 from django.core.management import BaseCommand
+from django.utils.module_loading import module_has_submodule
 from tabulate import tabulate
+
+from rele.config import load_subscriptions_from_paths
+
+
+def discover_subs_modules():
+    app_configs = apps.get_app_configs()
+    subs_modules = []
+    for conf in app_configs:
+        if module_has_submodule(conf.module, "subs"):
+            module = conf.name + ".subs"
+            subs_modules.append(module)
+    return subs_modules
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         headers = ['Topic', 'Subscriber(s)', 'Sub']
-        self.stdout.write(tabulate([], headers=headers))
+
+        subscription_paths = discover_subs_modules()
+        subs = sorted(load_subscriptions_from_paths(subscription_paths),
+                      key=lambda sub: sub.topic)
+        sub_data = [(sub.topic, sub.name, sub._func.__name__) for sub in subs]
+
+        self.stdout.write(tabulate(sub_data, headers=headers))
--- tests/commands/test_document.py
+++ tests/commands/test_document.py
@@ -1,14 +1,50 @@
 from unittest.mock import patch
 
+import pytest
 from django.core.management import call_command
+
+from rele.subscription import sub
+
+
+@sub(topic='some-cool-topic', prefix='rele')
+def sub_stub(data, **kwargs):
+    return data['id']
+
+
+@sub(topic='some-fancy-topic')
+def sub_fancy_stub(data, **kwargs):
+    return data['id']
+
+
+@sub(topic='published-time-type')
+def sub_published_time_type(data, **kwargs):
+    return f'{type(kwargs["published_at"])}'
+
+
+def landscape_filter(**kwargs):
+    return kwargs.get('type') == 'landscape'
+
+
+@sub(topic='photo-updated', filter_by=landscape_filter)
+def sub_process_landscape_photos(data, **kwargs):
+    return f'Received a photo of type {kwargs.get("type")}'
 
 
 class TestDocument:
 
     @patch('rele.management.commands.document.tabulate',
            autospec=True, return_value='')
-    def test_prints_table_when_called(self, mock_tabulate):
+    @patch('rele.management.commands.document.discover_subs_modules', return_value=[__name__])
+    def test_prints_table_when_called(self, mock_discover_subs, mock_tabulate):
         expected_headers = ['Topic', 'Subscriber(s)', 'Sub']
+        expected_subscription_data = [
+            ('photo-updated', 'photo-updated', 'sub_process_landscape_photos'),
+            ('published-time-type', 'published-time-type', 'sub_published_time_type'),
+            ('some-cool-topic', 'rele-some-cool-topic', 'sub_stub'),
+            ('some-fancy-topic', 'some-fancy-topic', 'sub_fancy_stub'),
+        ]
+
         call_command('document')
 
-        mock_tabulate.assert_called_once_with([], headers=expected_headers)
+        mock_tabulate.assert_called_once_with(expected_subscription_data, headers=expected_headers)
+        mock_discover_subs.assert_called_once()
