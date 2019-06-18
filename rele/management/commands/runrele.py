f6820e432ef71dbadc6b64c601650471c7c20766
Daniel Ruiz <druizca@mercadona.es>
2019-06-18 17:49:10 +0200

    Use discover function inside runrele command

--- rele/management/commands/runrele.py
+++ rele/management/commands/runrele.py
@@ -2,13 +2,13 @@ import logging
 import signal
 import time
 
-from django.apps import apps
 from django.conf import settings
 from django.core.management import BaseCommand
-from django.utils.module_loading import module_has_submodule
 
 from rele import Worker
 import rele
+
+from .discover import discover_subs_modules
 
 logger = logging.getLogger(__name__)
 
@@ -34,20 +34,9 @@ class Command(BaseCommand):
 
         self._wait_forever()
 
-    def _discover_subs_modules(self):
-        logger.debug('Autodiscovering subs...')
-        app_configs = apps.get_app_configs()
-        subs_modules = []
-        for conf in app_configs:
-            if module_has_submodule(conf.module, "subs"):
-                module = conf.name + ".subs"
-                subs_modules.append(module)
-                self.stdout.write(" * Discovered subs module: %r" % module)
-        return subs_modules
-
     def _autodiscover_subs(self):
         return rele.config.load_subscriptions_from_paths(
-            self._discover_subs_modules(),
+            discover_subs_modules(),
             settings.RELE['SUB_PREFIX'],
             settings.RELE.get('FILTER_SUBS_BY'))
 
