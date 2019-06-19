588e35f5da6dff35e8985a6a7a1e7ce7f748c3f6
Daniel Ruiz <daniel-ruiz@users.noreply.github.com>
2019-06-19 19:57:12 +0200

    Add showsubscriptions command (#73)
    * Move tests regarding commands into  package
    
    * Add tabulate as a dependency
    
    * Add command that only prints an empty table with the specified headers
    
    * Print information about loaded subscriptions in a project
    
    * Extract tabulate mock to a local fixture
    
    * Extract discover subs mock to a local fixture
    
    * Reformat code to fit coding style
    
    * Add help text
    
    * Move discover_subs_modules function to rele.management.commands.discover
    
    * Add logs to discover function
    
    * Use discover function inside runrele command
    
    * Change command name to showsubscriptions
    
    * Move discover module under rele.management
    
    - Django believes this module is another command
    
    * Assert command output to stdout
    
    * Shorten statement to conform linting rules
    
    * Update help text of showsubscriptions command

---
+++ tests/commands/test_showsubscriptions.py
@@ -0,0 +1,54 @@
+from unittest.mock import patch
+
+import pytest
+from django.core.management import call_command
+
+from rele.subscription import sub
+
+
+@sub(topic='some-cool-topic', prefix='rele')
+def sub_stub(data, **kwargs):
+    return data['id']
+
+
+@sub(topic='some-fancy-topic')
+def sub_fancy_stub(data, **kwargs):
+    return data['id']
+
+
+@sub(topic='published-time-type')
+def sub_published_time_type(data, **kwargs):
+    return f'{type(kwargs["published_at"])}'
+
+
+def landscape_filter(**kwargs):
+    return kwargs.get('type') == 'landscape'
+
+
+@sub(topic='photo-updated', filter_by=landscape_filter)
+def sub_process_landscape_photos(data, **kwargs):
+    return f'Received a photo of type {kwargs.get("type")}'
+
+
+@pytest.fixture()
+def mock_discover_subs():
+    affected_path = ('rele.management.commands.showsubscriptions'
+                     '.discover_subs_modules')
+    with patch(affected_path, return_value=[__name__]) as mock:
+        yield mock
+
+
+class TestShowSubscriptions:
+
+    def test_prints_table_when_called(self, capfd, mock_discover_subs):
+        call_command('showsubscriptions')
+
+        mock_discover_subs.assert_called_once()
+        captured = capfd.readouterr()
+        assert captured.out == '''Topic                Subscriber(s)         Sub
+-------------------  --------------------  ----------------------------
+photo-updated        photo-updated         sub_process_landscape_photos
+published-time-type  published-time-type   sub_published_time_type
+some-cool-topic      rele-some-cool-topic  sub_stub
+some-fancy-topic     some-fancy-topic      sub_fancy_stub
+'''
