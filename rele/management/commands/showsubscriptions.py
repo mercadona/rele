177f64e0d4abfd032748b1efcd229f15f97ad42e
Daniel Ruiz <daniel-ruiz@users.noreply.github.com>
2019-06-19 19:57:12 +0200

    Add showsubscriptions command (#73)
    * Move tests regarding commands into  package
    
    * Add tabulate as a dependency
    
    * Add command that only prints an empty table with the specified headers
    
    * Print information about loaded subscriptions in a project
    
    * Extract tabulate mock to a local fixture
    
    * Extract discover subs mock to a local fixture
    
    * Reformat code to fit coding style
    
    * Add help text
    
    * Move discover_subs_modules function to rele.management.commands.discover
    
    * Add logs to discover function
    
    * Use discover function inside runrele command
    
    * Change command name to showsubscriptions
    
    * Move discover module under rele.management
    
    - Django believes this module is another command
    
    * Assert command output to stdout
    
    * Shorten statement to conform linting rules
    
    * Update help text of showsubscriptions command

---
+++ rele/management/commands/showsubscriptions.py
@@ -0,0 +1,21 @@
+from django.core.management import BaseCommand
+from tabulate import tabulate
+
+from rele.config import load_subscriptions_from_paths
+
+from rele.management.discover import discover_subs_modules
+
+
+class Command(BaseCommand):
+    help = ('List information about Pub/Sub subscriptions '
+            'registered using Relé.')
+
+    def handle(self, *args, **options):
+        headers = ['Topic', 'Subscriber(s)', 'Sub']
+
+        subscription_paths = discover_subs_modules()
+        subs = sorted(load_subscriptions_from_paths(subscription_paths),
+                      key=lambda sub: sub.topic)
+        sub_data = [(sub.topic, sub.name, sub._func.__name__) for sub in subs]
+
+        self.stdout.write(tabulate(sub_data, headers=headers))
---
+++ rele/management/discover.py
@@ -0,0 +1,18 @@
+import logging
+
+from django.apps import apps
+from django.utils.module_loading import module_has_submodule
+
+logger = logging.getLogger(__name__)
+
+
+def discover_subs_modules():
+    logger.debug('Autodiscovering subs...')
+    app_configs = apps.get_app_configs()
+    subs_modules = []
+    for conf in app_configs:
+        if module_has_submodule(conf.module, "subs"):
+            module = conf.name + ".subs"
+            subs_modules.append(module)
+            logger.debug(" * Discovered subs module: %r" % module)
+    return subs_modules
