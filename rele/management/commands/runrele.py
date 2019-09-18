11a73f52e72bdedcf9e3925eb113c438a125260d
andrewgy8 <andrewgy8@gmail.com>
2019-09-18 17:26:38 +0200

    Check if max conn age is set appropriately

--- rele/management/commands/runrele.py
+++ rele/management/commands/runrele.py
@@ -15,12 +15,20 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Start subscriber threads to consume Relé topics."
+    help = "Start subscriber threads to consume messages from Relé topics."
     config = Config(settings.RELE)
 
     def handle(self, *args, **options):
+        if settings.CONN_MAX_AGE != 0:
+            self.stdout.write(self.style.WARNING(
+                'WARNING: settings.CONN_MAX_AGE is not set to 0. '
+                'This may result in slots for database connections to '
+                'be exhausted.'
+            ))
         subs = self._autodiscover_subs()
-        self.stdout.write(f"Configuring worker with {len(subs)} " f"subscription(s)...")
+        self.stdout.write(
+            f"Configuring worker with {len(subs)} " f"subscription(s)..."
+        )
         for sub in subs:
             self.stdout.write(f"  {sub}")
         worker = Worker(
