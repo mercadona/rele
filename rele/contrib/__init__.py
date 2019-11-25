a40440fd305aec72b84463ce4beaf8fc2b8b1cce
Craig Mulligan <hobochildster@gmail.com>
2019-11-25 18:10:00 +0000

    add flask middleware

--- rele/contrib/__init__.py
+++ rele/contrib/__init__.py
@@ -1,4 +1,5 @@
 from .logging_middleware import LoggingMiddleware  # noqa
+from .flask_middleware import FlaskMiddleware  # noqa
 
 try:
     from .django_db_middleware import DjangoDBMiddleware  # noqa
