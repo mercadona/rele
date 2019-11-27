4113b89d3a3c28dfad5564b0c1b949ae252fd30f
Craig Mulligan <hobochildster@gmail.com>
2019-11-27 08:35:03 +0000

    Rele flask (#127)

---
+++ tests/test_middleware.py
@@ -0,0 +1,26 @@
+import pytest
+import rele
+from unittest.mock import patch
+
+
+class TestMiddleware:
+    @pytest.fixture
+    def mock_init_global_publisher(self):
+        with patch("rele.config.init_global_publisher", autospec=True) as m:
+            yield m
+
+    @pytest.mark.usefixtures("mock_init_global_publisher")
+    @patch("rele.contrib.FlaskMiddleware.setup", autospec=True)
+    def test_setup_fn_is_called_with_kwargs(
+        self, mock_middleware_setup, project_id, credentials
+    ):
+
+        settings = {
+            "GC_PROJECT_ID": project_id,
+            "GC_CREDENTIALS": credentials,
+            "MIDDLEWARE": ["rele.contrib.FlaskMiddleware"],
+        }
+
+        rele.setup(settings, foo="bar")
+        assert mock_middleware_setup.called
+        mock_middleware_setup.call_args_list[0][-1] == {"foo": "bar"}
