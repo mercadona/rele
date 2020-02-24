b8a23588ad8d14f2c037f6047dfb6cf40ebac0de
Daniel Demmel <daniel.demmel@beryl.cc>
2020-02-24 15:38:41 +0000

    🔧 Last couple of adjustments

--- setup.py
+++ setup.py
@@ -11,9 +11,7 @@ def get_version(*file_paths):
     """Retrieves the version from rele/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M
-    )
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
