c33f1b0d0805871461d215fff4b4121768b5eab3
andrewgy8 <andrewgy8@gmail.com>
2020-06-11 15:12:26 +0200

    Add settings path to the cli

---
+++ tests/test_discover.py
@@ -0,0 +1,25 @@
+import pytest
+
+from rele import discover
+from tests import settings
+
+
+class TestDiscoverSubModules:
+
+    def test_returns_settings_and_paths_when_settings_found(self):
+        discovered_settings, paths = discover.sub_modules('tests.settings')
+
+        assert discovered_settings is settings
+        assert discovered_settings.RELE == settings.RELE
+        assert paths == ['tests.subs']
+
+    def test_returns_empty_settings_when_no_settings_module_found(self):
+        discovered_settings, paths = discover.sub_modules()
+
+        assert discovered_settings is None
+        assert paths == ['tests.subs']
+
+    def test_raises_when_incorrect_path(self):
+        incorrect_path = 'tests.foo'
+        with pytest.raises(ModuleNotFoundError):
+            discover.sub_modules(incorrect_path)
