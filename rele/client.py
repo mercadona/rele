defd2a965a434de80eb752abb14be94d649a4dbd
luisgc93 <luisgc93@gmail.com>
2020-07-01 19:08:26 +0200

    Remove unnecessary else statement

--- rele/client.py
+++ rele/client.py
@@ -171,8 +171,7 @@ class Publisher:
             run_middleware_hook("post_publish_failure", topic, e, data)
             if raise_exception:
                 raise e
-            else:
-                return False
+            return False
 
         run_middleware_hook("post_publish", topic)
         return future
