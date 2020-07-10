9d0dd4c4135477febbe3071208579e0c6d4b829d
Andrew Graham-Yooll <karlgrah@mercadona.es>
2020-07-10 17:45:24 +0200

    Remove GC_CREDENTIALS (#174)

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -13,13 +13,10 @@ class TestMiddleware:
 
     @pytest.mark.usefixtures("mock_init_global_publisher")
     @patch("rele.contrib.FlaskMiddleware.setup", autospec=True)
-    def test_setup_fn_is_called_with_kwargs(
-        self, mock_middleware_setup, project_id, credentials
-    ):
+    def test_setup_fn_is_called_with_kwargs(self, mock_middleware_setup, project_id):
 
         settings = {
             "GC_PROJECT_ID": project_id,
-            "GC_CREDENTIALS": credentials,
             "MIDDLEWARE": ["rele.contrib.FlaskMiddleware"],
         }
 
