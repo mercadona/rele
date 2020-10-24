9530ac5cc10601184b8ccdc363e112747bc29668
Luis Garcia Cuesta <luisgc93@gmail.com>
2020-10-24 15:00:21 +0200

    Revert "the docs show we can still get the topic path from the subscriber"
    This reverts commit 4bf64705026a4ad8f0fb74be35e0285ed72c019b.

--- rele/client.py
+++ rele/client.py
@@ -60,8 +60,8 @@ class Subscriber:
         subscription_path = self._client.subscription_path(
             self._gc_project_id, subscription
         )
-        subscriber = pubsub_v1.SubscriberClient()
-        topic_path = subscriber.topic_path(self._gc_project_id, topic)
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(self._gc_project_id, topic)
 
         with suppress(exceptions.AlreadyExists):
             try:
