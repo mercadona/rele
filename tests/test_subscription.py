5c18fb3e83e8eb1e354a957062826a8772a83768
Luis <32971373+luisgc93@users.noreply.github.com>
2021-02-18 10:07:31 +0100

    Google Pubsub 2.0 Compat (#192)
    * remove version pin
    
    * add publisher client fixture
    
    * The topic_path is now obtained from publisher client
    
    * update tests
    
    * Add publish_time to PubsubMessage object
    
    We need a Timestamp obj here with seconds and nanos attrs
    
    * we need to use ._pb not sure why tbh
    
    * fix last test
    
    * fix linting
    
    * fix import order
    
    * the docs show we can still get the topic path from the subscriber
    
    * update tests
    
    * force >=2.0 pubsub version
    
    * this is not needed
    
    * unused import
    
    * Revert "the docs show we can still get the topic path from the subscriber"
    
    This reverts commit 4bf64705026a4ad8f0fb74be35e0285ed72c019b.
    
    * Revert "this is not needed"
    
    This reverts commit 25629f67f874065c588bad6e7fe2dacc1eaee0f8.
    
    * Revert "update tests"
    
    This reverts commit 145ab6d459ea7c5e87c914e374579fbae1bb9a33.
    
    * Reverting this as it was fixed in v2.2.0
    
    * update pubsub pin
    
    * fix linting
    
    * fix import order
    
    Co-authored-by: Luis Garcia Cuesta <lgarccu@lgarccu-MacBook-Pro.local>

--- tests/test_subscription.py
+++ tests/test_subscription.py
@@ -5,6 +5,7 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 from google.cloud import pubsub_v1
+from google.protobuf import timestamp_pb2
 
 from rele import Callback, Subscription, sub
 from rele.middleware import register_middleware
@@ -144,15 +145,22 @@ class TestCallback:
         return time.time()
 
     @pytest.fixture
-    def message_wrapper(self, published_at):
+    def publish_time(self):
+        timestamp = timestamp_pb2.Timestamp()
+        timestamp.GetCurrentTime()
+        return timestamp
+
+    @pytest.fixture
+    def message_wrapper(self, published_at, publish_time):
         rele_message = pubsub_v1.types.PubsubMessage(
             data=b'{"id": 123}',
             attributes={"lang": "es", "published_at": str(published_at)},
             message_id="1",
+            publish_time=publish_time,
         )
 
         message = pubsub_v1.subscriber.message.Message(
-            rele_message,
+            rele_message._pb,
             "ack-id",
             delivery_attempt=1,
             request_queue=queue.Queue(),
@@ -172,12 +180,15 @@ class TestCallback:
         return message
 
     @pytest.fixture
-    def message_wrapper_invalid_json(self):
+    def message_wrapper_invalid_json(self, publish_time):
         rele_message = pubsub_v1.types.PubsubMessage(
-            data=b"foobar", attributes={}, message_id="1"
+            data=b"foobar",
+            attributes={},
+            message_id="1",
+            publish_time=publish_time,
         )
         message = pubsub_v1.subscriber.message.Message(
-            rele_message,
+            rele_message._pb,
             "ack-id",
             delivery_attempt=1,
             request_queue=queue.Queue(),
