68f52495ba4cd55af0d25d87528ebaf34e11e15b
Jordi Chulia <jchulia@mercadona.es>
2022-04-08 09:21:06 +0200

    Add VerboseLoggingMiddleware to contrib/__init__.py

--- rele/contrib/__init__.py
+++ rele/contrib/__init__.py
@@ -1,6 +1,7 @@
 from .flask_middleware import FlaskMiddleware  # noqa
 from .logging_middleware import LoggingMiddleware  # noqa
 from .unrecoverable_middleware import UnrecoverableMiddleWare  # noqa
+from .verbose_logging_middleware import VerboseLoggingMiddleware # noqa
 
 try:
     from .django_db_middleware import DjangoDBMiddleware  # noqa
