1b0fd821e0c7bc9494135718009dfc6fdf4ae610
Jordi Chulia <jorchube@users.noreply.github.com>
2022-04-13 09:37:00 +0200

    [Added] Added a Logging Middleware that does not truncate mesage payload. (#218)
    * Add verbose logging middleware
    
    * Add VerboseLoggingMiddleware to contrib/__init__.py
    
    * Fix linting

---
+++ rele/contrib/verbose_logging_middleware.py
@@ -0,0 +1,49 @@
+import json
+
+from rele.contrib.logging_middleware import LoggingMiddleware
+
+
+class VerboseLoggingMiddleware(LoggingMiddleware):
+    def post_process_message_failure(
+        self, subscription, exception, start_time, message
+    ):
+        super().post_process_message_failure(
+            subscription, exception, start_time, _VerboseMessage(message)
+        )
+
+    def post_publish_failure(self, topic, exception, message):
+        super().post_publish_failure(topic, exception, _VerboseMessage(message))
+
+
+class _VerboseMessage:
+    def __init__(self, message):
+        self._message = message
+        self.attributes = message.attributes
+
+    def __repr__(self):
+        _MESSAGE_REPR = """\
+Message {{
+  data: {!r}
+  ordering_key: {!r}
+  attributes: {}
+}}"""
+
+        data = self._message._message.data
+        attrs = self._message_attrs_repr()
+        ordering_key = str(self._message.ordering_key)
+
+        return _MESSAGE_REPR.format(data, ordering_key, attrs)
+
+    def _message_attrs_repr(self):
+        message_attrs = json.dumps(
+            dict(self.attributes), indent=2, separators=(",", ": "), sort_keys=True
+        )
+
+        indented = []
+        for line in message_attrs.split("\n"):
+            indented.append("  " + line)
+
+        message_attrs = "\n".join(indented)
+        message_attrs = message_attrs.lstrip()
+
+        return message_attrs
