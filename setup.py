a66ed8f6b89b2634b1125a469a696b0014a1b86e
Maks3w <github.maks3w@virtualplanets.net>
2022-10-22 13:20:56 +0200

    [setup] Add 3.10 compatibility identifier

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     entry_points={"console_scripts": ["rele-cli=rele.__main__:main"]},
 )
