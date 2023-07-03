eb64a7625fbd8bb8ab65175c1e2d9f0a63b12055
Rau <rausell99@gmail.com>
2023-07-03 17:37:40 +0200

    refactor

--- rele/management/discover.py
+++ rele/management/discover.py
@@ -7,28 +7,40 @@ from django.apps import apps
 logger = logging.getLogger(__name__)
 
 
-def collect_subs_paths(folder_path, subfiles, module_name, subs_paths):
+def is_subs_module(file):
+    return re.match(r"^(subs|subs\.py)$", file)
+
+
+def collect_subs_from_path(folder_path, subfiles, module_name, subs_paths):
     if not subfiles:
         return subs_paths
 
     file = subfiles.pop()
 
-    if re.match(r"^(subs|subs\.py)$", file):
+    if is_subs_module(file):
+        logger.debug(f" * Discovered subs module: {module_name}.subs")
         subs_paths.append(f"{module_name}.subs")
-        logger.debug(" * Discovered subs module: %r" % f"{module_name}.subs")
-        return collect_subs_paths(folder_path, subfiles, module_name, subs_paths)
+        return collect_subs_from_path(folder_path, subfiles, module_name, subs_paths)
 
     file_path = f"{folder_path}/{file}"
-    if os.path.isdir(file_path) and not re.match(r"^__pycache__$", file):
-        files_in_folder = os.listdir(file_path)
-        subs_paths = collect_subs_paths(
-            file_path,
-            files_in_folder,
-            f"{module_name}.{file}",
-            subs_paths
+    if os.path.isdir(file_path):
+        subs_paths = collect_subs_from_path(
+            folder_path=file_path,
+            subfiles=os.listdir(file_path),
+            module_name=f"{module_name}.{file}",
+            subs_paths=subs_paths
         )
 
-    return collect_subs_paths(folder_path, subfiles, module_name, subs_paths)
+    return collect_subs_from_path(folder_path, subfiles, module_name, subs_paths)
+
+
+def collect_subs_from_app(app_config):
+    return collect_subs_from_path(
+        folder_path=app_config.path,
+        subfiles=os.listdir(app_config.path),
+        module_name=app_config.name,
+        subs_paths=[]
+    )
 
 
 def discover_subs_modules():
@@ -36,9 +48,7 @@ def discover_subs_modules():
     app_configs = apps.get_app_configs()
     subs_modules = []
 
-    for conf in app_configs:
-        conf_path = conf.path
-        files_in_folder = os.listdir(conf_path)
-        subs_modules += collect_subs_paths(conf_path, files_in_folder, conf.name, [])
+    for app in app_configs:
+        subs_modules += collect_subs_from_app(app)
 
     return subs_modules
