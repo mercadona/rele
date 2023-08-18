acdc603c645a42c3c0ed5b65fed6124548a6512e
Andrew Graham-Yooll <andrewgy8@gmail.com>
2023-08-18 06:49:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -57,10 +57,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     entry_points={"console_scripts": ["rele-cli=rele.__main__:main"]},
 )
