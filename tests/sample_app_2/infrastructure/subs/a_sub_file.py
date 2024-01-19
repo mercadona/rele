1ca17423fcfae07218226b727d6575efe7405888
Awais <mawais@mercadona.es>
2024-01-19 16:50:38 +0100

    renaming

--- tests/sample_app_2/infrastructure/subs/a_sub_file.py
+++ tests/sample_app_2/infrastructure/subs/a_sub_file.py
@@ -1,6 +1,6 @@
 from rele import sub
 
 
-@sub(topic="sub-inside-a-module-2", prefix="rele")
+@sub(topic="sub-inside-a-module-infrastructure", prefix="rele")
 def sub_inside_a_module(data, **kwargs):
     return data["id"]
